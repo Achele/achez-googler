f5f112d15fc83a33bad36a3e5d15c606d78c345f
Ojoachele Onuh <ejigboojoachele@gmail.com>
2023-04-04 11:09:01 +0100

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p>Nigeria</p>
+      <div className="footer-bottom flex">
+        <ul className="footer1">
+          <li>About</li>
+          <li>Advertising</li>
+          <li>Business</li>
+          <li>How Search works</li>
+        </ul>
+        <p>Carbon natural since 2007</p>
+        <ul className="footer2">
+          <li>Privacy</li>
+          <li>Terms</li>
+          <li>Settings</li>
+        </ul>
+      </div>
+    </footer>
+  );
+}
