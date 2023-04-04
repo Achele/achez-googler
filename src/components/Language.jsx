d59c1d77dba5ae63989e72641b223b7746f2f1d6
Ojoachele Onuh <ejigboojoachele@gmail.com>
2023-04-04 11:09:16 +0100

    Create Language.jsx

---
+++ src/components/Language.jsx
@@ -0,0 +1,15 @@
+import "../App.css";
+
+export default function Language() {
+  return (
+    <div className="lang-wrapper">
+      <p>Google offered in: </p>
+      <ul className="languages">
+        <li>Hausa</li>
+        <li>Igbo</li>
+        <li>Édè Yorùbà</li>
+        <li>Nigerian Pidgin</li>
+      </ul>
+    </div>
+  );
+}
